8b8b72178890a5782bdf3fefb227cbee8a3fb0e5
Maxim Filin <m.a.f.max@mail.ru>
2023-12-01 15:34:33 +0500

    Поменял кодировку файла

--- Mafmax.BuildService/Mafmax.BuildService.Api/Controllers/FacadeController.cs
+++ Mafmax.BuildService/Mafmax.BuildService.Api/Controllers/FacadeController.cs
@@ -8,17 +8,17 @@ namespace Mafmax.BuildService.Api.Controllers;
 public class FacadeController(IFacadeCalculationService facadeCalculationService) : ControllerBase
 {
     /// <summary>
-    /// ¬ычисл€ет количество и длины фасадных панелей дл€ полного покрыти€ фасада здани€.
+    /// Вычисляет количество и длины фасадных панелей дл€ полного покрытия фасада здания.
     /// </summary>
-    /// <param name="facadeProfile">ќписание фасада здани€.</param>
+    /// <param name="facadeProfile">ќписание фасада здания.</param>
     [HttpPost("calculateCoverage")]
     public ValueTask<FacadeCoverageCalculationResult> CalculateFacadeCoverage([FromBody] FacadeProfile facadeProfile) =>
         ValueTask.FromResult(facadeCalculationService.CalculateFacadeCoverage(facadeProfile));
 
     /// <summary>
-    /// –аскраивает панели, минимизиру€ количество обрезков.
+    /// Раскраивает панели, минимизиру€ количество обрезков.
     /// </summary>
-    /// <param name="request">«апрос, содержащий описание досок, которые нужно получить в результате раскройки.</param>
+    /// <param name="request">Запрос, содержащий описание досок, которые нужно получить в результате раскройки.</param>
     [HttpPost("cutDesks")]
     public ValueTask<CutDesksResult> CutDesks([FromBody] CutDesksRequest request) => 
         ValueTask.FromResult(facadeCalculationService.CutDesks(request.Desks));
